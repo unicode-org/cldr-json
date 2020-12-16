5de1c46328ee4afa62bdb34c45b5c134c6392be9
Steven R. Loomis <srl295@gmail.com>
2020-12-16 15:28:00 -0600

    create zipfiles

--- cldr-generate-json.sh
+++ cldr-generate-json.sh
@@ -8,6 +8,7 @@
 CLDR_DIR=../cldr
 OUT=./cldr-json
 INDATA=../cldr-staging/production
+DIST=./dist
 
 # which types to generate
 TYPES="supplemental segments rbnf main annotations"
@@ -15,7 +16,7 @@ DRAFTSTATUS="contributed"
 VERSION="38.1.0-BETA3"
 
 # for now, seed has to exist.
-mkdir -p -v ${OUT} production/seed/main production/seed/annotations
+mkdir -p -v ${OUT} ${INDATA}/seed/main ${INDATA}/seed/annotations ${DIST}
 MAIN_CLASS=org.unicode.cldr.json.Ldml2JsonConverter
 export MAVEN_OPTS="-Xmx8192m -XX:MaxPermSize=1024m -Dexec.cleanupDaemonThreads=false -Dexec.mainClass=${MAIN_CLASS}"
 MVN="mvn --file=${CLDR_DIR}/tools/pom.xml"
@@ -28,6 +29,11 @@ ${MVN_COMPILE} || exit 1
 for type in ${TYPES}; do
     ${MVN_EXEC}  -Dexec.args="-p true -o false -r true -t ${type} -d ${OUT} -s ${DRAFTSTATUS} -V ${VERSION}" || exit 1
 done
+
+( cd ${OUT} && zip -r cldr-${VERSION}-json-full.zip ../LICENSE cldr-core cldr-rbnf cldr-*-full )
+( cd ${OUT} && zip -r cldr-${VERSION}-json-modern.zip ../LICENSE cldr-core cldr-rbnf cldr-*-modern )
+
+mv -v ${OUT}/*.zip ${DIST}/
 
 ## Example options for the generator:
 #[-p, true, -o, false, -r, true, -t, supplemental, -d, ./cldr-json, -s, contributed]
