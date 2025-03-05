0c052068432ad0fb4306d7e8e50571907e2d89a8
Steven R. Loomis <srl295@gmail.com>
2025-03-05 11:22:44 -0600

    CLDR-0000 branch allowing run from jar

--- cldr-generate-json.sh
+++ cldr-generate-json.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#     Copyright © 1991-2023 Unicode, Inc. All rights reserved. Distributed under
+#     Copyright © 1991-2025 Unicode, Inc. All rights reserved. Distributed under
 # the Terms of Use in http://www.unicode.org/copyright.html.
 #
 # Creates JSON data under ./cldr-json in this directory.
@@ -20,20 +20,33 @@ fi
 
 # for now, seed has to exist.
 mkdir -p -v ${OUT} ${INDATA}/seed/main ${INDATA}/seed/annotations ${DIST}
-MAIN_CLASS=org.unicode.cldr.json.Ldml2JsonConverter
-export MAVEN_OPTS="-Xmx16384m -Dexec.cleanupDaemonThreads=false -Dexec.mainClass=${MAIN_CLASS}"
-MVN="mvn ${MVN_OPTS} --file=${CLDR_DIR}/tools/pom.xml -pl cldr-code"
-MVN_COMPILE="${MVN} compile"
-MVN_EXEC="${MVN} -DCLDR_DIR=${INDATA} exec:java"
 
 set -x
-${MVN_COMPILE} || exit 1
+
+if [[ ! -f "${CLDR_JAR}" ]];
+then
+    MAIN_CLASS=org.unicode.cldr.json.Ldml2JsonConverter
+    export MAVEN_OPTS="-Xmx16384m -Dexec.cleanupDaemonThreads=false -Dexec.mainClass=${MAIN_CLASS}"
+    MVN="mvn ${MVN_OPTS} --file=${CLDR_DIR}/tools/pom.xml -pl cldr-code"
+    MVN_COMPILE="${MVN} compile"
+    MVN_EXEC="${MVN} -DCLDR_DIR=${INDATA} exec:java"
+    ${MVN_COMPILE} || exit 1
+else
+    echo "Using prebuilt jar ${CLDR_JAR}"
+    MVN_EXEC="java -DCLDR_DIR=${INDATA} -jar ${CLDR_JAR} ldml2json"
+fi
 
 for type in ${TYPES}; do
-    ${MVN_EXEC}  -Dexec.args="-m ${MATCH} -p true -o true -r true -t ${type} -d ${OUT} -s ${DRAFTSTATUS} -V ${VERSION} ${EXTRA_JSON_OPTS}" || exit 1
+    JSON_ARG="-m ${MATCH} -p true -o true -r true -t ${type} -d ${OUT} -s ${DRAFTSTATUS} -V ${VERSION} ${EXTRA_JSON_OPTS}"
+    if [[ ! -f "${CLDR_JAR}" ]];
+    then
+        ${MVN_EXEC}  "-Dexec.args=${JSON_ARG}" || exit 1
+    else
+        ${MVN_EXEC} ${JSON_ARG} || exit 1
+    fi
 done
 
-echo "Finshed converting '${TYPES}' to ${OUT}"
+echo "Finished converting '${TYPES}' to ${OUT}"
 
 if [ -x ./local-config.sh ];
 then
